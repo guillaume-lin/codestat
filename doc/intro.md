fb28e56062bb928b40a88983b4d67c747ddb3da6
Jingxian Lin <jingxian.lin@tpv-tech.com>
2015-03-05 09:11:09 +0800

    setup project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to codestat
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/codestat/core_test.clj
@@ -0,0 +1,7 @@
+(ns codestat.core-test
+  (:require [clojure.test :refer :all]
+            [codestat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
