5b4a0c2bfdcd0bda0f2f96b442c650d18e7a60e7
Jingxian Lin <jingxian.lin@tpv-tech.com>
2015-03-25 17:43:33 +0800

    add sample selenium code

--- src/codestat/core.clj
+++ src/codestat/core.clj
@@ -1,8 +1,20 @@
 (ns codestat.core
   (:gen-class))
+(use 'clj-webdriver.taxi)
+(defn test-selenium[]
+ (do
+   ;; Start up a browser
+   (set-driver! {:browser :chrome} "https://github.com")
+   (click "a[href*='login']")
+   (input-text "#login_field" "your-username")
+   (input-text "#password" "your-password")
+   (submit "#password")
+   (quit)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (test-selenium))
+
 
