8c770ee560c4fa0e24cdaec62d939c6234e894cf
Jingxian Lin <jingxian@gmail.com>
2015-04-04 22:31:15 +0800

    add db

--- src/codestat/mysql.clj
+++ src/codestat/mysql.clj
@@ -9,27 +9,52 @@
                   :password "codestat"}))
 (use 'korma.core)
 
-;;;
-;;; define database record here
-;;; gitlog [id name date revision commit_message change_line  project_url]
-;;; svnlog [id name date revision log_message change_line project_url ]
-;;; urtlog [id name date issue_no operator operate_type project_url]
-;;;
-;;; create table gitlog(id int primary key auto_increment,
-;;;               name varchar(20), commit_date timestamp , revision varchar(40), 
-;;;               commit_message varchar(80), change_line int, project_url varchar(80));
-;;;
-;;; create table svnlog(id int primary key auto_increment,
-;;;               name varchar(20), commit_date timestamp, revision int,
-;;;               log_message varchar(80), change_line int, project_url varchar(80));
-;;;
-;;; create table urtlog(id int primary key auto_increment,
-;;;               name varchar(20), operate_date timestamp, issue_no int,
-;;;               operator varchar(20), operator_type varchar(10), project_url varchar(80));
-;;;
+(comment 
+drop table gitlog;
+create table gitlog(id int primary key auto_increment,
+               name varchar(20), 
+               commit_date timestamp , 
+               revision varchar(40), 
+               commit_message varchar(80), 
+               add_line int , 
+               delete_line int, 
+               project_url varchar(80));
+
+drop table svnlog;
+create table svnlog(id int primary key auto_increment,
+               name varchar(20), 
+               commit_date timestamp, 
+               revision int,
+               log_message varchar(80), 
+               add_line int, 
+               delete_line int, 
+               project_url varchar(80));
+
+drop table urtlog;
+create table urtlog(id int primary key auto_increment,
+               name varchar(20), 
+               operate_date timestamp, 
+               issue_no int,
+               issue_title varchar(80),
+               operator varchar(20), 
+               operator_type varchar(10), 
+               project_url varchar(80));
+
+)
+
+
 (defentity gitlog)
 (defentity svnlog)
 (defentity urtlog)
+
+(defrecord gitlog_rec 
+  [name,commit_date,revision, commit_message,
+   add_line,delete_line,project_url ])
+(defn insert-gitlog
+  [rec ]
+(insert gitlog 
+        (values rec)))
+
 
 
 
