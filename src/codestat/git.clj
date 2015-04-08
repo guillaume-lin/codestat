90e3f6624302c92e4da838558bd9a150209ee223
Jingxian Lin <jingxian.lin@tpv-tech.com>
2015-04-08 17:43:55 +0800

    change database table

--- src/codestat/git.clj
+++ src/codestat/git.clj
@@ -18,16 +18,71 @@
 ;;; 
 ;;; return a line-seq of git logs
 ;;;
-(defn git-log
+(defn git-log-1
   [dir]
   (line-seq (java.io.BufferedReader. 
               (java.io.StringReader. 
                 (:out (with-sh-dir dir
-                        (sh "git" "log" "--shortstat"))))))
+                        (sh "git" "log" "--numstat" "--format='Author:%an%nDate:%at%nCommit:%H%nMessage:%s'")))))))
 ;;;
 (defn get-branches
   [project-url]
   ())
+
+;;; 
+;;; log format as:
+;;; git log --numstat --format="Author:%an%nDate:%at%nCommit:%H%nMessage:%s"
+;;;
+(comment
+Author:lx.mao
+Date:1428461520
+Commit:85fa48a1409bb22b70b9d4d6173766d7777e741d
+Message:[TvApp]Show channel logo and current program's thumbnail on channel list.
+
+6       0       source/TvApp/res/layout/channel_item.xml
+4       2       source/TvApp/res/values/dimens.xml
+8       3       source/TvApp/src/org/droidtv/playtvapp/browse/ChannelAdapter.java
+20      16      source/TvApp/src/org/droidtv/playtvapp/browse/ChannelItemView.java
+0       7       source/TvApp/src/org/droidtv/playtvapp/browse/SourceBrowser.java
+1       1       source/TvApp/src/org/droidtv/playtvapp/data/Channel.java
+5       0       source/TvApp/src/org/droidtv/playtvapp/data/Data.java
+Author:Jonathan Jeurissen
+Date:1428422271
+Commit:2e99f048810717b7a49eb328cac95c87bc4691c8
+Message:[ReacTV] Renamed resource file for build issue
+
+-       -       source/ReacTV/res/drawable/App_icon_original.png
+-       -       source/ReacTV/res/drawable/app_icon_original.png
+Author:Jonathan Jeurissen
+
+)
+(defn get-commit-revision
+  [line]
+  (if-let [ret (clojure.string/split line #":")]
+    (if (= (nth ret 0) "Commit")
+      (nth ret 1))))
+(defn get-commit-author
+  [line]
+  (if-let [ret (clojure.string/split line #":")]
+    (if (= (nth ret 0) "Author")
+      (nth ret 1))))
+(defn get-commit-date
+  [line]
+  (if-let [ret (clojure.string/split line #":")]
+    (if (= (nth ret 0) "Date")
+      (nth ret 1))))
+(defn get-commit-message
+  [line]
+  (if-let [ret (clojure.string/split line #":")]
+    (if (= (nth ret 0) "Message")
+      (nth ret 1))))
+
+;;; return add delete file vector
+(defn get-change-file
+  [line]
+  (if-let [ret (clojure.string/split line #"\t")]
+    ret))
+
 
 ;;;=============================================
 ;;; data manipulation
@@ -40,8 +95,10 @@
   ())
 
 (defn insert-git-log
-  []
+  [a]
   ())
 
 (defn count-by-author-change-line
+  [b]
   ())
+
--- src/codestat/mysql.clj
+++ src/codestat/mysql.clj
@@ -10,26 +10,21 @@
 (use 'korma.core)
 
 (comment 
-drop table gitlog;
-create table gitlog(id int primary key auto_increment,
-               name varchar(20), 
+drop table commitlog;
+create table commitlog(id int primary key auto_increment,
+               author varchar(20), 
                commit_date timestamp , 
                revision varchar(40), 
-               commit_message varchar(80), 
-               add_line int , 
-               delete_line int, 
+               commit_message varchar(80),
                project_url varchar(80));
 
-drop table svnlog;
-create table svnlog(id int primary key auto_increment,
-               name varchar(20), 
-               commit_date timestamp, 
-               revision int,
-               log_message varchar(80), 
-               add_line int, 
-               delete_line int, 
-               project_url varchar(80));
-
+drop table changeset;
+create table changeset(id int primary key auto_increment,
+                          commit_id int,
+                          add_line int,
+                          delete_line int,
+                          file varchar(80));
+                          
 drop table urtlog;
 create table urtlog(id int primary key auto_increment,
                name varchar(20), 
