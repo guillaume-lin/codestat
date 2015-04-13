dc1c40581daccb86e3efc906dbf86bfb31bd39b3
Jingxian Lin <jingxian.lin@tpv-tech.com>
2015-04-13 15:24:23 +0800

    add web front end

--- src/codestat/web.clj
+++ src/codestat/web.clj
@@ -1,5 +1,6 @@
 (ns codestat.web
-  (:use (clojure.java io)))
+  (:use (clojure.java io))
+  (:use (codestat mysql git)))
 
 ;;;
 ;;; web interface for code stat
@@ -8,22 +9,8 @@
 	"get the body"
 	[body]
 	(let [ret (line-seq (reader body))]
-		(spit "/tmp/req" ret)
+		(spit (str "/tmp/" (gensym)) ret)
 		ret))
-
-
-(defn handler
-   "ring handler"
-   [req]
-   (let [ret (str "uri:" (:uri req) 
-                  "\nscheme:" (:scheme req)
-                  "\nrequest-method:" (:request-method req)
-                  "\nbody:" (get-body (:body req)))]
-
-   {:status 200
-    :headers {"Content-Type" "text/plain"}
-    :body ret 
-	}))
 
 ;;;
 ;;; add a project to the database
@@ -31,9 +18,14 @@
 ;;;
 (defn add-project
   [req]
+  (insert-project 
+    (->project-rec 
+      0 "project_name" "project_desc"
+      "vcs_url2" "vcs_login2" "vcs_pass2"
+      "issue_url3" "issue_login3" "issue_pass3"))
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body ""})
+   :body "add-project"})
 
 ;;;
 ;;; return project list
@@ -41,7 +33,41 @@
 ;;;
 (defn get-project
   [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body ""})
+  (let [sb (StringBuffer.)]
+    (.append sb "<html><body><table>")
+    (doseq [p (query-project)]
+      (-> sb (.append "<tr>")
+        (.append  "<td>") (.append (:project_name p)) (.append "</td>") 
+        (.append "<td>") (.append (:vcs_url p)) (.append "</td>") 
+        (.append "<td>") (.append (:issue_url p)) (.append "</td>") 
+        (.append "</tr>")))
+    (.append sb "</table></body></html>")
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (.toString sb)}))
+
+;;;
+;;; define handler map
+;;;
+(def ^:dynamic *handler-map* 
+  (hash-map "/project.list" get-project
+            "/project.add" add-project))
+
+
+;;;
+;;; the dispatcher
+;;;
+(defn handler
+   "ring handler"
+   [req]
+   (if-let [hlr (get *handler-map* (:uri req))]
+     (hlr req)
+   (let [ret (str "uri:" (:uri req) 
+             "\nscheme:" (:scheme req)
+             "\nrequest-method:" (:request-method req)
+             "\nbody:" (get-body (:body req)))]
+
+   {:status 200
+    :headers {"Content-Type" "text/plain"}
+    :body ret })))
   
